d0deb71aa6ca0360a13b0ad142682838698910e4
wlwaq <yaoyao@ciotc.org>
2013-08-07 10:11:46 +0800

    20130807lsc

--- src/ae.c
+++ src/ae.c
@@ -10,13 +10,13 @@
 #include "ae.h"
 
 
-/* define HOME to be dir for key and cert files... */
-#define HOME "./"
-/* Make these what you want for cert & key files */
-#define CACERTF  HOME "demoCA/cacert.pem"
-#define CAKEYF  HOME "demoCA/private/cakey.pem"
-#define CLIENTCERTF  HOME "demoCA/newcerts/usercert2.pem"
-#define CLIENTKEYF  HOME "userkey2.pem"
+///* define HOME to be dir for key and cert files... */
+//#define HOME "./"
+///* Make these what you want for cert & key files */
+//#define CACERTF  HOME "demoCA/cacert.pem"
+//#define CAKEYF  HOME "demoCA/private/cakey.pem"
+//#define CLIENTCERTF  HOME "demoCA/newcerts/usercert2.pem"
+//#define CLIENTKEYF  HOME "userkey2.pem"
 //#define PrivKey_PWD 111111
 
 #define CHK_NULL(x) if ((x)==NULL) exit (1)
@@ -190,11 +190,9 @@ BOOL getCertData(int userID, BYTE buf[], int *len)
 	memset(certname, '\0', sizeof(certname));//初始化certname,以免后面写如乱码到文件中
 
 	if (userID == 0)
-		//sprintf(certname, "./demoCA/cacert.pem");//./demoCA/
-		sprintf(certname, "./cacert.pem");//./demoCA/
+		sprintf(certname, "./newcerts/cacert.pem");
 	else
-		//sprintf(certname, "./demoCA/newcerts/usercert%d.pem", certnum);  //终端运行./client
-		sprintf(certname, "./usercert%d.pem", userID);                //eclipse调试或运行
+		sprintf(certname, "./newcerts/usercert%d.pem", userID);                //eclipse调试或运行
 
 	printf("cert file name: %s\n", certname);
 
@@ -219,11 +217,9 @@ BOOL writeCertFile(int userID, BYTE buf[], int len)
 	memset(certname, '\0', sizeof(certname));//初始化certname,以免后面写如乱码到文件中
 
 	if (userID == 0)
-		//sprintf(certname, "./demoCA/cacert.pem");//./demoCA/
-		sprintf(certname, "./cacert.pem");//./demoCA/
+		sprintf(certname, "./newcerts/cacert.pem");
 	else
-		//sprintf(certname, "./demoCA/newcerts/usercert%d.pem", certnum);  //终端运行./client
-		sprintf(certname, "./usercert%d.pem", userID);                //eclipse调试或运行
+		sprintf(certname, "./newcerts/usercert%d.pem", userID);                //eclipse调试或运行
 
 	printf("cert file name: %s\n", certname);
 
@@ -241,7 +237,18 @@ BOOL writeCertFile(int userID, BYTE buf[], int len)
 	return TRUE;
 }
 
+/*************************************************
 
+Function:    // getprivkeyfromprivkeyfile
+Description: // CA(驻留在ASU中)从cakey.pem中提取CA的私钥，以便后续进行ASU的签名
+Calls:       // openssl读取私钥PEM文件相关函数
+Called By:   // 待添加！！！
+Input:	     //	无
+Output:      //	CA(驻留在ASU中)的私钥
+Return:      // EVP_PKEY *privKey
+Others:      // 该函数只是在本工程中为asu.c专用，即提取CA(驻留在ASU中)的私钥，如需提取其他私钥，还有待于将打开文件的目录及文件名做点修改
+
+*************************************************/
 EVP_PKEY * getprivkeyfromprivkeyfile(int userID)
 {
 	EVP_PKEY * privKey;
@@ -251,9 +258,10 @@ EVP_PKEY * getprivkeyfromprivkeyfile(int userID)
 	char keyname[40];
 
 	if (userID == 0)
-		sprintf(keyname, "./cakey.pem");//./demoCA/
+		sprintf(keyname, "./private/cakey.pem");
 	else
-		sprintf(keyname, "./userkey%d.pem", userID);                //eclipse调试或运行
+		sprintf(keyname, "./private/userkey%d.pem", userID);
+
 	fp = fopen(keyname, "r");
 
 	printf("key file name: %s\n", keyname);
@@ -294,7 +302,7 @@ EVP_PKEY * getprivkeyfromprivkeyfile(int userID)
 Function:    // getpubkeyfromcert
 Description: // 从数字证书(PEM文件)中读取公钥
 Calls:       // openssl中读PEM文件的API
-Called By:   // fill_certificate_auth_resp_packet
+Called By:   // 待添加！！！
 Input:	     //	用户证书的用户名certnum
 Output:      //	数字证书公钥
 Return:      // EVP_PKEY *pubKey
@@ -312,9 +320,9 @@ EVP_PKEY *getpubkeyfromcert(int certnum)
 	char certname[60];
 	memset(certname, '\0', sizeof(certname)); //初始化certname,以免后面写如乱码到文件中
 	if (certnum == 0)
-		sprintf(certname, "./demoCA/cacert.pem"); //./demoCA/
+		sprintf(certname, "./newcerts/cacert.pem"); //./demoCA/
 	else
-		sprintf(certname, "./demoCA/newcerts/usercert%d.pem", certnum);
+		sprintf(certname, "./newcerts/usercert%d.pem", certnum);
 
 	BIO_read_filename(key,certname);
 	if (!PEM_read_bio_X509(key, &Cert, 0, NULL))
@@ -335,7 +343,7 @@ EVP_PKEY *getpubkeyfromcert(int certnum)
 Function:    // verify_sign
 Description: // 验证数字签名
 Calls:       // openssl验证签名的API
-Called By:   // fill_certificate_auth_resp_packet
+Called By:   // 待添加！！！
 Input:	     //	input---待验证签名的整个数据包
                 sign_input_len---待验证签名的有效数据字段的长度，并非整个input长度
                 sign_value---签名字段
@@ -385,6 +393,134 @@ BOOL verify_sign(BYTE *input,int sign_input_len,BYTE * sign_value, unsigned int 
 	EVP_MD_CTX_cleanup(&mdctx);
 	return TRUE;
 }
+
+
+/*************************************************
+
+Function:    // SHA256
+Description: // SHA256散列函数
+Calls:       // openssl SHA256的API函数
+Called By:   //
+Input:	     //	input---待计算摘要的输入数据
+                input_len---待计算摘要的输入数据长度
+                output---摘要结果输出
+Output:      //	摘要值
+Return:      // 256bit(32Byte)摘要
+Others:      // 本处注释只是为了大家理解，待理解后，本处注释可删除
+
+*************************************************/
+//SHA256(input, input_len, output);
+
+
+/*************************************************
+
+Function:    // hmac_sha256
+Description: // WAPI消息认证MAC算法
+Calls:       // openssl SHA256的API函数
+Called By:   // 待添加！！！
+Input:	     //	text---待计算MAC的输入数据
+                text_len---待计算MAC的输入数据长度
+                key---hmac密钥
+                key_len---hmac密钥长度
+                digest---输出MAC值
+Output:      //	MAC值
+Return:      // 256bit(32Byte)MAC
+Others:      // 如果想设定输出MAC的长度，可考虑添加一个输出MAC长度的形参
+
+*************************************************/
+
+void hmac_sha256(
+		const BYTE *text,      /* pointer to data stream        */
+		int        text_len,   /* length of data stream         */
+		const BYTE *key,       /* pointer to authentication key */
+		int        key_len,    /* length of authentication key  */
+		void       *digest)    /* caller digest to be filled in */
+{
+	BYTE k_ipad[65]; /* inner padding -
+	                  * key XORd with ipad
+	                  */
+	BYTE k_opad[65]; /* outer padding -
+	                  * key XORd with opad
+	                  */
+	BYTE tk[SHA256_DIGEST_LENGTH];
+	BYTE tk2[SHA256_DIGEST_LENGTH];
+	BYTE bufferIn[1024];
+	BYTE bufferOut[1024];
+	int i;
+	/* if key is longer than 64 bytes reset it to key=sha256(key) */
+	if (key_len > 64)
+	{
+		SHA256(key, key_len, tk);
+		key = tk;
+		key_len = SHA256_DIGEST_LENGTH;
+	}
+	/*
+	 * the HMAC_SHA256 transform looks like:
+	 *
+	 * SHA256(K XOR opad, SHA256(K XOR ipad, text))
+	 *
+	 * where K is an n byte key
+	 * ipad is the byte 0x36 repeated 64 times
+	 * opad is the byte 0x5c repeated 64 times
+	 * and text is the data being protected
+	 */
+	/* start out by storing key in pads */
+	memset(k_ipad, 0, sizeof k_ipad);
+	memset(k_opad, 0, sizeof k_opad);
+	memcpy(k_ipad, key, key_len);
+	memcpy(k_opad, key, key_len);
+
+	/* XOR key with ipad and opad values */
+	for (i = 0; i < 64; i++)
+	{
+		k_ipad[i] ^= 0x36;
+		k_opad[i] ^= 0x5c;
+	}
+	/*
+	 * perform inner SHA256
+	 */
+	memset(bufferIn, 0x00, 1024);
+	memcpy(bufferIn, k_ipad, 64);
+	memcpy(bufferIn + 64, text, text_len);
+	SHA256(bufferIn, 64 + text_len, tk2);
+	/*
+	 * perform outer SHA256
+	 */
+	memset(bufferOut, 0x00, 1024);
+	memcpy(bufferOut, k_opad, 64);
+	memcpy(bufferOut + 64, tk2, SHA256_DIGEST_LENGTH);
+	SHA256(bufferOut, 64 + SHA256_DIGEST_LENGTH, digest);
+}
+/*************************************************
+
+Function:    // gen_randnum
+Description: // 生成随机数
+Calls:       // openssl SHA256的API函数以及RAND_bytes函数
+Called By:   // 待添加！！！
+Input:	     //	randnum---保存生成的随机数
+                randnum_len---随机数长度
+Output:      //	随机数
+Return:      // 256bit(32Byte)MAC
+Others:      //
+
+*************************************************/
+void gen_randnum(BYTE *randnum,int randnum_len)
+{
+	int ret;
+	BYTE randnum_seed[randnum_len];
+
+	ret = RAND_bytes(randnum_seed, randnum_len);
+	if(ret!=1)
+	{
+		printf("生成随机数种子失败！\n");
+	}
+	//参考WAPI实施指南P49 SHA-256(挑战种子)--->挑战，随机数生成算法：SHA-256(随机数种子)--->随机数
+	SHA256(randnum_seed, randnum_len, randnum);
+}
+
+
+
+
 int getECDHparam(ecdh_param *ecdhparam, const char *oid)
 {
 	unsigned char  *buf; 
@@ -533,13 +669,6 @@ BOOL gen_sign(BYTE * input,int sign_input_len,BYTE * sign_value, unsigned int *s
 	EVP_MD_CTX_cleanup(&mdctx);
 	return TRUE;
 }
-
-int par_certificate_auth_resp_packet(certificate_auth_requ * cert_auth_resp_buffer_recv)
-{
-	return TRUE;
-}
-
-
 
 //1) ProcessWAPIProtocolAuthActive
 int fill_auth_active_packet(int user_ID,auth_active *auth_active_packet)
@@ -766,6 +895,7 @@ int fill_certificate_auth_requ_packet(int user_ID,certificate_auth_requ *certifi
 	certificate_auth_requ_packet->wai_packet_head.type = 1;
 	certificate_auth_requ_packet->wai_packet_head.subtype = CERTIFICATE_AUTH_REQU;
 	certificate_auth_requ_packet->wai_packet_head.reserved = 0;
+	certificate_auth_requ_packet->wai_packet_head.length = sizeof(certificate_auth_requ);
 	certificate_auth_requ_packet->wai_packet_head.packetnumber = 3;
 	certificate_auth_requ_packet->wai_packet_head.fragmentnumber = 0;
 	certificate_auth_requ_packet->wai_packet_head.identify = 0;
@@ -805,9 +935,6 @@ int fill_certificate_auth_requ_packet(int user_ID,certificate_auth_requ *certifi
 
 	certificate_auth_requ_packet->staaecer.cer_length = cert_len;   //证书长度字段
 	memcpy((certificate_auth_requ_packet->staaecer.cer_X509),cert_buffer, cert_len);
-
-	//fill packet length
-	certificate_auth_requ_packet->wai_packet_head.length = sizeof(certificate_auth_requ);
 
 	//fill ae signature
 	EVP_PKEY * privKey;
@@ -852,7 +979,7 @@ int ProcessWAPIProtocolCertAuthRequest(int user_ID,certificate_auth_requ *certif
 
 int HandleProcessWAPIProtocolCertAuthResp(int user_ID, certificate_auth_resp *certificate_auth_resp_packet,access_auth_resp *access_auth_resp_packet)
 {
-	//read ca certificate get ca pubkey(公钥)
+	//读取CA(驻留在ASU)中的公钥证书获取CA公钥
 	EVP_PKEY *asupubKey = NULL;
 	BYTE *pTmp = NULL;
 	BYTE derasupubkey[1024];
@@ -869,18 +996,35 @@ int HandleProcessWAPIProtocolCertAuthResp(int user_ID, certificate_auth_resp *ce
 	}
 	printf("\n");
 
-	//verify asu's sign
+	//验证ASU服务器对整个证书认证响应分组(除本字段外)的签名，检验该分组的完整性、验证该份组的发送源身份
 	if (verify_sign((BYTE *) certificate_auth_resp_packet,
 			sizeof(certificate_auth_resp) - sizeof(sign_attribute),
-			certificate_auth_resp_packet->asusign.sign.data,
-			certificate_auth_resp_packet->asusign.sign.length, asupubKey))
+			certificate_auth_resp_packet->cerauthrespasusign.sign.data,
+			certificate_auth_resp_packet->cerauthrespasusign.sign.length, asupubKey))
 	{
-		printf("验证ASU签名正确......\n");
+		printf("验证ASU服务器对整个证书认证响应分组(除本字段外)的签名正确！！！......\n");
 		EVP_PKEY_free(asupubKey);
 	}
 
-	//read certificate_auth_resp_packet cervalidresult,and copy the access_auth_resp_packet cervalidresult
-	memcpy(&(access_auth_resp_packet->cervalidresult),&(certificate_auth_resp_packet->cervalidresult),sizeof(certificate_valid_result));
+
+
+//	//验证ASU服务器对证书验证结果字段的签名
+//	if (verify_sign((BYTE *) &(certificate_auth_resp_packet->cervalidresult),
+//			sizeof(certificate_valid_result),
+//			certificate_auth_resp_packet->cervalresasusign.sign.data,
+//			certificate_auth_resp_packet->cervalresasusign.sign.length, asupubKey))
+//	{
+//		printf("验证ASU服务器对证书验证结果字段的签名正确！！！......\n");
+//		EVP_PKEY_free(asupubKey);
+//	}
+
+
+
+	//读取证书认证响应分组中的证书验证结果字段，将该字段拷贝到接入认证响应分组中的复合证书验证结果的证书验证结果字段中
+	memcpy(&(access_auth_resp_packet->cervalrescomplex.ae_asue_cert_valid_result),&(certificate_auth_resp_packet->cervalidresult),sizeof(certificate_valid_result));
+
+	//读取证书认证响应分组中的ASU服务器对证书验证结果字段的签名字段，将该字段拷贝到接入认证响应分组中的复合证书验证结果的签名字段中
+	memcpy(&(access_auth_resp_packet->cervalrescomplex.ae_asue_cert_valid_result_asu_sign),&(certificate_auth_resp_packet->cervalresasusign),sizeof(certificate_valid_result));
 	return TRUE;
 
 }
@@ -971,7 +1115,8 @@ int ProcessWAPIProtocolAccessAuthResp(int user_ID, access_auth_resp *access_auth
 	return TRUE;
 }
 
-void ProcessWAPIProtocol(int new_asue_socket)
+//void ProcessWAPIProtocol(int new_asue_socket)
+void ProcessWAPIProtocol()
 {
 	int user_ID = 2;
 	int asu_socket;
@@ -982,25 +1127,25 @@ void ProcessWAPIProtocol(int new_asue_socket)
 	access_auth_resp access_auth_resp_packet;
 	
 
-	//1) ProcessWAPIProtocolAuthActive
-	printf("***\n 1) ProcessWAPIProtocolAuthActive: \n");
-	ProcessWAPIProtocolAuthActive(user_ID, &auth_active_packet);
-	printf("auth_active_packet:\n");
-	int i;
-	for(i=0; i<sizeof(auth_active_packet); i++)
-		printf("%x", ((BYTE *)&auth_active_packet)[i]);
-	printf("\n");
-	send_to_peer(new_asue_socket, (BYTE *)&auth_active_packet, sizeof(auth_active_packet));
-
-	//2) ProcessWAPIProtocolAccessAuthRequest
-	printf("***\n 2) HandleWAPIProtocolAccessAuthRequest: \n");
-	memset((BYTE *)&access_auth_requ_packet, 0, sizeof(access_auth_requ));
-	printf("recv auth active packet from ASUE...\n");
-	recv_from_peer(new_asue_socket, (BYTE *)&access_auth_requ_packet, sizeof(access_auth_requ_packet));
-
-
-	//verify access_auth_requ_packet
-	HandleWAPIProtocolAccessAuthRequest(user_ID, &access_auth_requ_packet);
+//	//1) ProcessWAPIProtocolAuthActive
+//	printf("***\n 1) ProcessWAPIProtocolAuthActive: \n");
+//	ProcessWAPIProtocolAuthActive(user_ID, &auth_active_packet);
+//	printf("auth_active_packet:\n");
+//	int i;
+//	for(i=0; i<sizeof(auth_active_packet); i++)
+//		printf("%x", ((BYTE *)&auth_active_packet)[i]);
+//	printf("\n");
+//	send_to_peer(new_asue_socket, (BYTE *)&auth_active_packet, sizeof(auth_active_packet));
+//
+//	//2) ProcessWAPIProtocolAccessAuthRequest
+//	printf("***\n 2) HandleWAPIProtocolAccessAuthRequest: \n");
+//	memset((BYTE *)&access_auth_requ_packet, 0, sizeof(access_auth_requ));
+//	printf("recv auth active packet from ASUE...\n");
+//	recv_from_peer(new_asue_socket, (BYTE *)&access_auth_requ_packet, sizeof(access_auth_requ_packet));
+//
+//
+//	//verify access_auth_requ_packet
+//	HandleWAPIProtocolAccessAuthRequest(user_ID, &access_auth_requ_packet);
 	
 	//3) ProcessWAPIProtocolCertAuthRequest
 	printf("connect to asu.\n");
@@ -1019,10 +1164,10 @@ void ProcessWAPIProtocol(int new_asue_socket)
 
 
 
-	//5) ProcessWAPIProtocolAccessAuthResp
-	printf("***\n 5) ProcessWAPIProtocolAccessAuthResp: \n");
-	ProcessWAPIProtocolAccessAuthResp(user_ID, &access_auth_resp_packet);
-	send_to_peer(new_asue_socket, (BYTE *)&access_auth_resp_packet, sizeof(access_auth_resp_packet));
+//	//5) ProcessWAPIProtocolAccessAuthResp
+//	printf("***\n 5) ProcessWAPIProtocolAccessAuthResp: \n");
+//	ProcessWAPIProtocolAccessAuthResp(user_ID, &access_auth_resp_packet);
+//	send_to_peer(new_asue_socket, (BYTE *)&access_auth_resp_packet, sizeof(access_auth_resp_packet));
 	
 }
 
@@ -1127,9 +1272,10 @@ int main(int argc, char **argv)
 	 printf("res = %d\n",(int)res);
 	 
 */
-	printf("listen from asue.\n");
-	listen_from_asue();
+//	printf("listen from asue.\n");
+//	listen_from_asue();
 
+	ProcessWAPIProtocol();
 	return 0;
 
 }
