5a99a5bf7f50dfd405ed879654041657942ac19e
wlwaq <yaoyao@ciotc.org>
2013-08-08 17:17:14 +0800

    20130808lsc

--- src/ae.c
+++ src/ae.c
@@ -961,6 +961,7 @@ int fill_certificate_auth_requ_packet(int user_ID,access_auth_requ *access_auth_
 	//fill asue certificate
 
 	memcpy(&(certificate_auth_requ_packet->staasuecer),&(access_auth_requ_packet->certificatestaasue),sizeof(certificate));
+	//memset((BYTE *)&(certificate_auth_requ_packet->staasuecer),0,sizeof(certificate));
 
 	//fill ae certificate
 	BYTE cert_buffer[5000];
@@ -1230,7 +1231,7 @@ void ProcessWAPIProtocol(int new_asue_socket)
 	printf("***\n 5) ProcessWAPIProtocolAccessAuthResp: \n");
 	ProcessWAPIProtocolAccessAuthResp(user_ID, &access_auth_requ_packet, &access_auth_resp_packet);
 	send_to_peer(new_asue_socket, (BYTE *)&access_auth_resp_packet, sizeof(access_auth_resp_packet));
-	
+
 }
 
 void * serve_each_asue(void * new_server_socket_to_client)
